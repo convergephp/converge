6f5385b389527bad007815d2f859725df871667b
CharrafiMed <mhch5003@gmail.com>
2025-01-10 21:30:25 +0100

    wip

--- tests/Unit/FileTreeBuilderTest.php
+++ tests/Unit/FileTreeBuilderTest.php
@@ -5,82 +5,93 @@ use Fluxtor\Converge\FilesTreeBuilder;
 
 $stubs = realpath(__DIR__ . '/../stubs/docs');
 
+it('build the correct file tree', function () use ($stubs) {
+    [$tree, $map] = FilesTreeBuilder::build($stubs);
 
-[$tree, $map] = FilesTreeBuilder::build($stubs);
-
-$expectedTree = [
-    [
-        "title" => "Installation",
-        "path" => "{$stubs}/01-installation.md",
-        "type" => "file",
-        "url" => "installation"
-    ],
-    [
-        "title" => "Getting started",
-        "path" => "{$stubs}/02-getting-started.md",
-        "type" => "file",
-        "url" => "getting-started"
-    ],
-    [
-        "title" => "Fields",
-        "path" => "{$stubs}/03-fields",
-        "type" => "folder",
-        "children" => [
-            [
-                "title" => "Getting started",
-                "path" => "{$stubs}/03-fields/01-getting-started.md",
-                "type" => "file",
-                "url" => "getting-started"
-            ],
-            [
-                "title" => "Text input",
-                "path" => "{$stubs}/03-fields/02-text-input.md",
-                "type" => "file",
-                "url" => "fields/text-input"
-            ],
-            [
-                "title" => "Select",
-                "path" => "{$stubs}/03-fields/03-select.md",
-                "type" => "file",
-                "url" => "fields/select"
+    $expectedTree = [
+        [
+            "title" => "Installation",
+            "path" => "{$stubs}/01-installation.md",
+            "type" => "file",
+            "url" => "installation"
+        ],
+        [
+            "title" => "Getting started",
+            "path" => "{$stubs}/02-getting-started.md",
+            "type" => "file",
+            "url" => "getting-started"
+        ],
+        [
+            "title" => "Fields",
+            "path" => "{$stubs}/03-fields",
+            "type" => "folder",
+            "children" => [
+                [
+                    "title" => "Getting started",
+                    "path" => "{$stubs}/03-fields/01-getting-started.md",
+                    "type" => "file",
+                    "url" => "getting-started"
+                ],
+                [
+                    "title" => "Text input",
+                    "path" => "{$stubs}/03-fields/02-text-input.md",
+                    "type" => "file",
+                    "url" => "fields/text-input"
+                ],
+                [
+                    "title" => "Select",
+                    "path" => "{$stubs}/03-fields/03-select.md",
+                    "type" => "file",
+                    "url" => "fields/select"
+                ]
+            ]
+        ],
+        [
+            "title" => "Layout",
+            "path" => "{$stubs}/04-layout",
+            "type" => "folder",
+            "children" => [
+                [
+                    "title" => "Getting started",
+                    "path" => "{$stubs}/04-layout/01-getting-started.md",
+                    "type" => "file",
+                    "url" => "layout/getting-started"
+                ],
+                [
+                    "title" => "Grid",
+                    "path" => "{$stubs}/04-layout/02-grid.md",
+                    "type" => "file",
+                    "url" => "layout/grid"
+                ],
+                [
+                    "title" => "Fieldset",
+                    "path" => "{$stubs}/04-layout/03-fieldset.md",
+                    "type" => "file",
+                    "url" => "layout/fieldset"
+                ]
             ]
         ]
-    ],
-    [
-        "title" => "Layout",
-        "path" => "{$stubs}/04-layout",
-        "type" => "folder",
-        "children" => [
-            [
-                "title" => "Getting started",
-                "path" => "{$stubs}/04-layout/01-getting-started.md",
-                "type" => "file",
-                "url" => "layout/getting-started"
-            ],
-            [
-                "title" => "Grid",
-                "path" => "{$stubs}/04-layout/02-grid.md",
-                "type" => "file",
-                "url" => "layout/grid"
-            ],
-            [
-                "title" => "Fieldset",
-                "path" => "{$stubs}/04-layout/03-fieldset.md",
-                "type" => "file",
-                "url" => "layout/fieldset"
-            ]
-        ]
-    ]
-];
-$expectedMap = [
-    "installation" => "{$stubs}/01-installation.md",
-    "getting-started" => "{$stubs}/02-getting-started.md",
-    "fields/getting-started" => "{$stubs}/03-fields/01-getting-started.md",
-    "fields/text-input" => "{$stubs}/03-fields/02-text-input.md",
-    "fields/select" => "{$stubs}/03-fields/03-select.md",
-    "layout/getting-started" => "{$stubs}/04-layout/01-getting-started.md",
-    "layout/grid" => "{$stubs}/04-layout/02-grid.md",
-    "layout/fieldset" => "{$stubs}/04-layout/03-fieldset.md"
-];
+    ];
+    $expectedMap = [
+        "installation" => "{$stubs}/01-installation.md",
+        "getting-started" => "{$stubs}/02-getting-started.md",
+        "fields/getting-started" => "{$stubs}/03-fields/01-getting-started.md",
+        "fields/text-input" => "{$stubs}/03-fields/02-text-input.md",
+        "fields/select" => "{$stubs}/03-fields/03-select.md",
+        "layout/getting-started" => "{$stubs}/04-layout/01-getting-started.md",
+        "layout/grid" => "{$stubs}/04-layout/02-grid.md",
+        "layout/fieldset" => "{$stubs}/04-layout/03-fieldset.md"
+    ];
+    
+    // Assert that the generated tree structure matches the expected one
+    expect($tree)->toEqual($expectedTree);
 
-dd($tree);
+    // Assert that the generated map matches the expected map
+    expect($map)->toEqual($expectedMap);
+});
+
+
+
+
+
+dd($tree, $map);
