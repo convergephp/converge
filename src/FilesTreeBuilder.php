b4b8ca1fe48a70291e1463cb09f248abe8e46287
CharrafiMed <mhch5003@gmail.com>
2025-01-10 19:27:48 +0100

    wip

--- src/FilesTreeBuilder.php
+++ src/FilesTreeBuilder.php
@@ -32,9 +32,60 @@ final class FilesTreeBuilder
             RecursiveDirectoryIterator::SKIP_DOTS
         );
         foreach ($iterator as $fileInfo) {
-            // process file item
+            $relativePath = str_replace($path, '', $fileInfo->getRealPath());
+            $relativePath = ltrim($relativePath, DIRECTORY_SEPARATOR);
+            $title = self::formatTitle($fileInfo->getBasename());
+
+            if ($fileInfo->isDir()) {
+                $tree[] = [
+                    'type' => 'folder',
+                    'title' => $title,
+                    'path' => $fileInfo->getRealPath(),
+                    'children' => self::tree($fileInfo->getRealPath()),
+                ];
+            } elseif ($fileInfo->isFile()) {
+                $url = self::generateUrl($relativePath);
+
+                // Add file to the tree
+                $tree[] = [
+                    'type' => 'file',
+                    'title' => $title,
+                    'path' => $fileInfo->getRealPath(),
+                    'url' => $url,
+                ];
+
+                // Map the URL to the file path
+                self::$urlToPathMap[$url] = $fileInfo->getRealPath();
+            }
         }
 
-        return [];
+        return $tree;
+    }
+
+    private static function formatTitle(string $basename): string
+    {
+        // Convert filename to a user-friendly title
+        $title = ucfirst(preg_replace('/^\d+-?/', '', pathinfo($basename, PATHINFO_FILENAME)));
+        $title = str_replace('-', ' ', $title);
+        return $title;
+    }
+
+    public static function generateUrl(string $relativePath)
+    {
+        $path = str_replace(DIRECTORY_SEPARATOR, '/', $relativePath);
+
+        // Split the path into segments using explode   
+        $segments = explode('/', $path);
+
+        // Process each segment to remove numeric prefixes
+        $segments = array_map(fn($segment) => preg_replace('/^\d+-?/', '', $segment), $segments);
+
+        // Join the processed segments back into a path
+        $url = implode('/', $segments);
+
+        // Remove the file extension, if anyr
+        $url = preg_replace('~\.[^\.]+$~', '', $url);
+
+        return $url;
     }
 }
