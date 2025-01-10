0b4cb422df03bc3882df3989cbdd5442dfa88cce
CharrafiMed <mhch5003@gmail.com>
2025-01-10 23:44:44 +0100

    fix final class rule in pint

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,4 @@ namespace Fluxtor\Converge\Tests;
 
 use PHPUnit\Framework\TestCase as FrameworkTestCase;
 
-final class TestCase extends FrameworkTestCase {}
+class TestCase extends FrameworkTestCase {}
