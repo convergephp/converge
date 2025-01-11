fcad513ef93da5a655aad682ba7be74f56ae811f
CharrafiMed <mhch5003@gmail.com>
2025-01-11 16:20:34 +0100

    wip

--- src/Providers/ConvergeServiceProvider.php
+++ src/Providers/ConvergeServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Fluxtor\Converge;
+namespace Fluxtor\Converge\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -11,5 +11,6 @@ class ConvergeServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/converge.php' => config_path('converge.php'),
         ]);
+        $this->app->register(provider: RouteServiceProvider::class, force: true);
     }
 }
---
+++ src/Providers/RouteServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fluxtor\Converge\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as baseServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+final class RouteServiceProvider extends baseServiceProvider
+{
+    public function boot(): void
+    {
+        $this->routes(function () {
+            Route::middleware('web')->group(__DIR__ . '/../routes/web.php');
+        });
+    }
+}
