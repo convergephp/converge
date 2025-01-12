591ac1d515832e4c5002e4c4cae076b3343d1cf4
CharrafiMed <mhch5003@gmail.com>
2025-01-12 23:45:49 +0100

    wip

--- resources/views/components/sidebar/group/index.blade.php
+++ resources/views/components/sidebar/group/index.blade.php
@@ -1,19 +1,21 @@
-@props(['navItems' => null])
+@props([
+    'groupItem' => null,
+])
+
 @php
-    use App\Support\Packages\NavigationGroup;    
-    use App\Support\Packages\NavigationItem;    
+    use App\Support\Packages\NavigationGroup;
+    use App\Support\Packages\NavigationItem;
 @endphp
 
-<ul class="bg-gray-100 dark:bg-black">
-    @foreach ($navItems as $item)
+<li class="mt-12 lg:mt-8">
+    <h5 class="mb-3.5 font-semibold text-gray-900 lg:mb-2.5 dark:text-gray-200">
+        {{ $groupItem->getLabel() }}
+    </h5>
+    @foreach ($groupItem->getItems() as $item)
         @if ($item instanceof NavigationItem)
-         <x-packages.sidebar.item :label="$item->getLabel()" :url="$item->getUrl()"/>   
-        @elseif($item instanceof NavigationGroup || count($item->getItems()>=1))
-        <li class="mt-12 lg:mt-8">
-            <h5 class="mb-3.5  font-semibold text-gray-900 lg:mb-2.5 dark:text-gray-200">
-                {{ $item->getLabel() }}
-            </h5>
-            <x-packages.sidebar.sub-nav-items :items="$item->getItems()"/>
+            <x-converge::sidebar.item :label="$item->getLabel()" :url="$item->getUrl()" />
+        @elseif($item instanceof NavigationGroup || count($item->getItems() >= 1))
+            <x-converge::sidebar.group :groupItem="$item" />
         @endif
     @endforeach
-</ul>
+</li>
