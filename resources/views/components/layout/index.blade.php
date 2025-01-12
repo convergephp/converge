9a0246603f70f9553533ca6765eed080a74be0cd
CharrafiMed <mhch5003@gmail.com>
2025-01-12 19:24:49 +0100

    wip

--- resources/views/components/layout/index.blade.php
+++ resources/views/components/layout/index.blade.php
@@ -3,66 +3,42 @@
 ])
 
 <!DOCTYPE html>
-<html
-    lang="{{ str_replace('_', '-', app()->getLocale()) }}"
-    {{-- dir="" --}}
-    @class([
-        'fi min-h-screen',
-        'dark' => filament()->hasDarkModeForced(),
-    ])
->
-    <head>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-        <meta charset="utf-8" />
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+<head>
 
-        {{-- @if ($favicon = filament()->getFavicon())
+    <meta charset="utf-8" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    {{-- @if ($favicon = filament()->getFavicon())
             <link rel="icon" href="{{ $favicon }}" /> todo
         @endif --}}
 
-        <title>
-            {{-- {{ filled($title) ? "{$title} - " : null }} {{ $brandName }} todo --}}
-        </title>
+    <title>
+        {{-- {{ filled($title) ? "{$title} - " : null }} {{ $brandName }} todo --}}
+    </title>
 
+    <style>
+        [x-cloak] {
+        }
+    </style>
 
-        <style>
-            [x-cloak=''],
-            [x-cloak='x-cloak'],
-            [x-cloak='1'] {
-                display: none !important;
-            }
+    {{-- {{ filament()->getTheme()->getHtml() }} --}}
+    {{-- {{ filament()->getFontHtml() }} --}}
 
-            @media (max-width: 1023px) {
-                [x-cloak='-lg'] {
-                    display: none !important;
-                }
-            }
+    <style>
+        :root {
+            /* --font-family: '{!! filament()->getFontFamily() !!}'; todo */
+            /* --sidebar-width: {{ filament()->getSidebarWidth() }}; todo */
+            /* --collapsed-sidebar-width: {{ filament()->getCollapsedSidebarWidth() }}; todo */
+            /* --default-theme-mode: {{ filament()->getDefaultThemeMode()->value }}; todo */
+        }
+    </style>
 
-            @media (min-width: 1024px) {
-                [x-cloak='lg'] {
-                    display: none !important;
-                }
-            }
-        </style>
+    @stack('styles')
 
-        {{-- {{ filament()->getTheme()->getHtml() }} --}}
-        {{-- {{ filament()->getFontHtml() }} --}} 
-
-        <style>
-            :root {
-                /* --font-family: '{!! filament()->getFontFamily() !!}'; todo */ 
-                /* --sidebar-width: {{ filament()->getSidebarWidth() }}; todo */
-                /* --collapsed-sidebar-width: {{ filament()->getCollapsedSidebarWidth() }}; todo */
-                /* --default-theme-mode: {{ filament()->getDefaultThemeMode()->value }}; todo */
-            }
-        </style>
-
-        @stack('styles')
-
-        {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::STYLES_AFTER, scopes: $livewire->getRenderHookScopes()) }}
-
-        {{-- @if (! filament()->hasDarkMode())
+    {{-- @if (!filament()->hasDarkMode())
             <script>
                 localStorage.setItem('theme', 'light')
             </script>
@@ -71,29 +47,29 @@
                 localStorage.setItem('theme', 'dark')
             </script>
         @else --}}
-            <script>
-                const theme = localStorage.getItem('theme') ?? @js(filament()->getDefaultThemeMode()->value)
+    <script>
+        const theme = localStorage.getItem('theme') ?? @js(filament()->getDefaultThemeMode()->value)
 
-                if (
-                    theme === 'dark' ||
-                    (theme === 'system' &&
-                        window.matchMedia('(prefers-color-scheme: dark)')
-                            .matches)
-                ) {
-                    document.documentElement.classList.add('dark')
-                }
-            </script>
-        {{-- @endif --}}
-    </head>
+        if (
+            theme === 'dark' ||
+            (theme === 'system' &&
+                window.matchMedia('(prefers-color-scheme: dark)')
+                .matches)
+        ) {
+            document.documentElement.classList.add('dark')
+        }
+    </script>
+    {{-- @endif --}}
+</head>
 
-    <body
-        {{ $attributes
-                ->class([
-                    'converge-body',
-                    'converge-panel-' . filament()->getId(),
-                    'min-h-screen bg-gray-50 font-normal text-gray-950 antialiased dark:bg-gray-950 dark:text-white',
-                ]) }}
-    >
-        {{ $slot }}
-    </body>
+<body
+    {{ $attributes->class([
+        'converge-body',
+        'min-h-screen bg-gray-50 font-normal text-gray-950 antialiased dark:bg-gray-950 dark:text-white',
+    ]) }}>
+    {{ $slot }}
+</body>
+
+@stack('scripts')
+
 </html>
