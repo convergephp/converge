244e2bcc55f1b0e7d7d59e1c790bec6287ec049c
CharrafiMed <mhch5003@gmail.com>
2025-01-12 23:52:58 +0100

    wip

--- resources/views/components/layout/index.blade.php
+++ resources/views/components/layout/index.blade.php
@@ -1,7 +1,12 @@
 <x-converge::layout.base>
     <div>
-        <x-navbar/>
+        <x-navbar />
+        <x-converge::sidebar :$navItems />
 
-        {{ $slot }}
+        <div class="lg:pl-[10.125rem]" x-on:scroll.window="handlePermalinks" x-data="tableOfContents">
+            <div class="mx-auto max-w-3xl pt-10 xl:ml-0 xl:mr-[10.5rem] xl:max-w-none xl:pr-12"></div>
+            {{ $slot }}
+            <x-converge::table-of-contents />
+        </div>
     </div>
 </x-converge::layout.base>
---
+++ resources/views/components/table-of-contents/index.blade.php
@@ -0,0 +1,11 @@
+<div 
+    class="right-side fixed bottom-0 right-[max(0px,calc(50%-45rem))] top-[3.8125rem]  mt-8 hidden w-[17.5rem] overflow-y-auto py-10 text-gray-400 xl:block" 
+>
+    <div>
+        <span class="text-primary">on this page</span>
+        <div 
+            class="prose prose-invert prose-a:text-gray-200 prose-headings:text-[10em]" 
+            x-html="$data.tableOfContent"
+        ></div>
+    </div>
+</div>
