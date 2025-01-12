04ee2b1ae829e4f0e31138bbd1e6b41b1b3f09b4
CharrafiMed <mhch5003@gmail.com>
2025-01-12 16:16:13 +0100

    wip

--- tests/Unit/NavigationBuilderTest.php
+++ tests/Unit/NavigationBuilderTest.php
@@ -2,81 +2,127 @@
 
 declare(strict_types=1);
 
-use Fluxtor\Converge\FilesTreeBuilder;
+use Fluxtor\Converge\Navigation\NavigationBuilder;
+use Fluxtor\Converge\Navigation\NavigationGroup;
+use Fluxtor\Converge\Navigation\NavigationItem;
+use Illuminate\Support\Collection;
 
 $stubs = realpath(__DIR__ . '/../stubs/docs');
+/**
+ * similate the generated tree from the Fluxtor\Converge\FilesTreeBuilder class
+ */
+$generatedTree = [
+    [
+        'label' => 'Installation',
+        'path' => 'docs/01-installation.md',
+        'type' => 'file',
+        'url' => 'installation',
+    ],
+    [
+        'label' => 'Getting started',
+        'path' => 'docs/02-getting-started.md',
+        'type' => 'file',
+        'url' => 'getting-started',
+    ],
+    [
+        'label' => 'Fields',
+        'type' => 'folder',
+        'children' => [
+            [
+                'label' => 'Text Input',
+                'path' => 'docs/03-fields/01-text-input.md',
+                'type' => 'file',
+                'url' => 'fields/text-input',
+            ],
+            [
+                'label' => 'Select',
+                'path' => 'docs/03-fields/02-select.md',
+                'type' => 'file',
+                'url' => 'fields/select',
+            ],
+            [
+                'label' => 'Sub Fields',
+                'type' => 'folder',
+                'children' => [
+                    [
+                        'label' => 'Text Input',
+                        'path' => 'docs/03-fields/01-sub-fields/01-text-input.md',
+                        'type' => 'file',
+                        'url' => 'fields/sub-fields/text-input',
+                    ],
+                    [
+                        'label' => 'Select',
+                        'path' => 'docs/03-fields/01-sub-fields/02-select.md',
+                        'type' => 'file',
+                        'url' => 'fields/sub-fields/select',
+                    ],
+                ],
+            ],
+        ],
 
-// it('build the correct navigations items', function () use ($stubs) {
-//     [$tree] = FilesTreeBuilder::build($stubs)[0];
+    ],
+];
 
-//     // Normalize the paths in the expected results to use forward slashes
-//     $normalize = fn($path) => str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $path);
+it('build the correct navigations items', function () use ($generatedTree) {
+    // Simulate max depth 2  by truncating children beyond depth 2
+    // dd($generatedTree);
+    $navItems = NavigationBuilder::build($generatedTree);
+    expect($navItems)->toBeInstanceOf(Collection::class);
+    expect($navItems->count())->toBe(3);
+    // validate the files 
+    $file = $navItems->get(0);
+    expect($file)->toBeInstanceOf(NavigationItem::class);
+    expect($file->getlabel())->toBe('Installation');
+    expect($file->getpath())->toBe('docs/01-installation.md');
+    expect($file->geturl())->toBe('installation');
+    expect($file->getDepth())->toBe(0);
 
-//     $expectedTree = [
-//         [
-//             'label' => 'Installation',
-//             'path' => $normalize("{$stubs}/01-installation.md"),
-//             'type' => 'file',
-//             'url' => 'installation',
-//         ],
-//         [
-//             'label' => 'Getting started',
-//             'path' => $normalize("{$stubs}/02-getting-started.md"),
-//             'type' => 'file',
-//             'url' => 'getting-started',
-//         ],
-//         [
-//             'label' => 'Fields',
-//             'path' => $normalize("{$stubs}/03-fields"),
-//             'type' => 'folder',
-//             'children' => [
-//                 [
-//                     'label' => 'Getting started',
-//                     'path' => $normalize("{$stubs}/03-fields/01-getting-started.md"),
-//                     'type' => 'file',
-//                     'url' => 'fields/getting-started',
-//                 ],
-//                 [
-//                     'label' => 'Text input',
-//                     'path' => $normalize("{$stubs}/03-fields/02-text-input.md"),
-//                     'type' => 'file',
-//                     'url' => 'fields/text-input',
-//                 ],
-//                 [
-//                     'label' => 'Select',
-//                     'path' => $normalize("{$stubs}/03-fields/03-select.md"),
-//                     'type' => 'file',
-//                     'url' => 'fields/select',
-//                 ],
-//             ],
-//         ],
-//         [
-//             'label' => 'Layout',
-//             'path' => $normalize("{$stubs}/04-layout"),
-//             'type' => 'folder',
-//             'children' => [
-//                 [
-//                     'label' => 'Getting started',
-//                     'path' => $normalize("{$stubs}/04-layout/01-getting-started.md"),
-//                     'type' => 'file',
-//                     'url' => 'layout/getting-started',
-//                 ],
-//                 [
-//                     'label' => 'Grid',
-//                     'path' => $normalize("{$stubs}/04-layout/02-grid.md"),
-//                     'type' => 'file',
-//                     'url' => 'layout/grid',
-//                 ],
-//                 [
-//                     'label' => 'Fieldset',
-//                     'path' => $normalize("{$stubs}/04-layout/03-fieldset.md"),
-//                     'type' => 'file',
-//                     'url' => 'layout/fieldset',
-//                 ],
-//             ],
-//         ],
-//     ];
+    // validate the group
+    $group = $navItems->get(2);
+    expect($group)->toBeInstanceOf(NavigationGroup::class);
+    expect($group->getLabel())->toBe('Fields');
+    expect($group->getItems())->toBeInstanceOf(Collection::class);
+    expect($group->getItems()->count())->toBe(3);
+    expect($group->getDepth())->toBe(0);
 
-//     expect($tree)->toEqual($expectedTree);
-// });
+    // validate the group childrens 
+    $children = $group->getItems()->get(0);
+    expect($children)->toBeInstanceOf(NavigationItem::class);
+    expect($children->getlabel())->toBe('Text Input');
+    expect($children->getpath())->toBe('docs/03-fields/01-text-input.md');
+    expect($children->geturl())->toBe('fields/text-input');
+    expect($children->getDepth())->toBe(1);
 
+    // sub children 
+    $subGroup = $group->getItems()->get(2);
+    expect($subGroup)->toBeInstanceOf(NavigationGroup::class);
+    expect($subGroup->getItems()->get(0)->getDepth())->toBe(2);
+    // enouuuuuuuuuuuuuuugh tests please for recursive, mohamed
+});
+
+it('respect the max depth = 2 provided', function () use ($generatedTree) {
+    $generatedTree[2]['children'][2]['children'] = [];
+    $navItems = NavigationBuilder::build($generatedTree);
+    // dd($navItems);
+    expect($navItems)->toBeInstanceOf(Collection::class);
+    expect($navItems->count())->toBe(3);
+    // validate the files 
+    $file = $navItems->get(0);
+    expect($file)->toBeInstanceOf(NavigationItem::class);
+    expect($file->getlabel())->toBe('Installation');
+    expect($file->getpath())->toBe('docs/01-installation.md');
+    expect($file->geturl())->toBe('installation');
+    expect($file->getDepth())->toBe(0);
+
+    $group = $navItems->get(2);
+    expect($group)->toBeInstanceOf(NavigationGroup::class);
+    expect($group->getLabel())->toBe('Fields');
+    expect($group->getItems())->toBeInstanceOf(Collection::class);
+    expect($group->getItems()->count())->toBe(2)->not->toBe(3); // if the children of a group empty it need to remove the group entirely 
+    expect($group->getDepth())->toBe(0);
+
+    // Ensure "Sub Fields" is not included at depth 1
+    $subGroup = $group->getItems()->get(1); // It should not be here in this depth
+    expect($subGroup)->toBeInstanceOf(NavigationItem::class); // If it is here, it should be an item not a group
+    expect($subGroup->getLabel())->toBe('Select'); // Make sure it's just the file, not a group
+});
