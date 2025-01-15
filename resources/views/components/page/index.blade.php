20ab2d22e24556c64086c58be6dd30f0723f1ed5
CharrafiMed <mhch5003@gmail.com>
2025-01-16 00:44:46 +0100

    add typography plugin

--- resources/views/components/page/index.blade.php
+++ resources/views/components/page/index.blade.php
@@ -1,8 +1,8 @@
 <x-converge::layout>
     <div class="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
         <x-converge::sidebar/>
-        <div class="lg:pl-[12.125rem]">
-            <div class="mx-auto max-w-3xl pt-10 xl:ml-0 xl:mr-[10.5rem] xl:max-w-none xl:pr-12">
+        <div class="lg:pl-[15.125rem]">
+            <div class="mx-auto max-w-3xl pt-10 xl:ml-0 xl:mr-[10.5rem] xl:max-w-none xl:pr-14">
                 {{ $slot }}
             </div>
             <x-converge::table-of-contents />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import typography from "@tailwindcss/typography";
 module.exports = {
-  darkMode: "selector",
-  content: ["./resources/views/**/*.blade.php", "./src/**/*.php"],
-  theme: {
-    extend: {
-      colors: {
-        gray: {
-          950: '#000000',
+    darkMode: "selector",
+    content: ["./resources/views/**/*.blade.php", "./src/**/*.php"],
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    950: "#000000",
+                },
+            },
         },
-      }
     },
-  },
-  plugins: [],
+    plugins: [typography],
 };
