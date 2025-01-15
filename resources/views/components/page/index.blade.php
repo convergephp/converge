7c57cb4c71478149619c58559ec6deeb6ca4d865
CharrafiMed <mhch5003@gmail.com>
2025-01-16 00:14:11 +0100

    wip

--- resources/views/components/page/index.blade.php
+++ resources/views/components/page/index.blade.php
@@ -1,9 +1,10 @@
 <x-converge::layout>
-    <div>
+    <div class="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
         <x-converge::sidebar/>
-        <div class="lg:pl-[10.125rem]">
-            <div class="mx-auto max-w-3xl pt-10 xl:ml-0 xl:mr-[10.5rem] xl:max-w-none xl:pr-12"></div>
-            {{ $slot }}
+        <div class="lg:pl-[12.125rem]">
+            <div class="mx-auto max-w-3xl pt-10 xl:ml-0 xl:mr-[10.5rem] xl:max-w-none xl:pr-12">
+                {{ $slot }}
+            </div>
             <x-converge::table-of-contents />
         </div>
     </div>
