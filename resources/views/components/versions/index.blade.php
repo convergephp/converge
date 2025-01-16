d35b0b12b15c6e4c4e877c034c412642e9969795
CharrafiMed <mhch5003@gmail.com>
2025-01-16 22:32:49 +0100

    wip

--- resources/views/components/versions/index.blade.php
+++ resources/views/components/versions/index.blade.php
@@ -24,7 +24,7 @@
             }  
         }">
         <x-converge::dropdown position="bottom-right"> 
-            <x-slot:button class="text-xs leading-5 font-semibold bg-white dark:bg-white/5 rounded-full py-1 px-3 flex items-center space-x-2 hover:bg-gray-100 dark:hover:bg-white/[0.08] text-gray-800 dark:text-gray-200 ">
+            <x-slot:button class="text-xs leading-5 font-semibold bg-gray-200 hover:opacity-80 transition-all duration-300 dark:bg-white/5 rounded-full py-1 px-3 flex items-center space-x-2  dark:hover:bg-white/[0.08] text-gray-800 dark:text-gray-200 ">
                   <span>v3.0.4</span>
                   <svg width="6" height="3" class="ml-2 overflow-visible" aria-hidden="true"><path d="M0 0L3 3L6 0" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path></svg>
             </x-slot:button>
