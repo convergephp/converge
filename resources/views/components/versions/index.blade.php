4cde1b8944cfe92f039e78ddf2ee064e8f71fd2d
CharrafiMed <mhch5003@gmail.com>
2025-01-16 22:28:29 +0100

    wip

--- resources/views/components/versions/index.blade.php
+++ resources/views/components/versions/index.blade.php
@@ -9,22 +9,20 @@
     </label>
 
     <div x-data="{
-        theme: null,
-        init() {
-            this.theme = localStorage.getItem('theme')
-            this.$watch('theme', () => {
-                $dispatch('theme-changed', this.theme)
-            })
-        },
-        setTheme(val){
-            this.theme = val;
-        }, 
-        themeIs(mode){
-            return this.theme === mode;
-        }  
-    }">
-
-
+            theme: null,
+            init() {
+                this.theme = localStorage.getItem('theme')
+                this.$watch('theme', () => {
+                    $dispatch('theme-changed', this.theme)
+                })
+            },
+            setTheme(val){
+                this.theme = val;
+            }, 
+            themeIs(mode){
+                return this.theme === mode;
+            }  
+        }">
         <x-converge::dropdown position="bottom-right"> 
             <x-slot:button class="text-xs leading-5 font-semibold bg-white dark:bg-white/5 rounded-full py-1 px-3 flex items-center space-x-2 hover:bg-gray-100 dark:hover:bg-white/[0.08] text-gray-800 dark:text-gray-200 ">
                   <span>v3.0.4</span>
@@ -33,7 +31,7 @@
             <x-slot:items class="w-36 dark:bg-transparent bg-gray-100">
                 @foreach($versions as $version)
                     <x-converge::dropdown.item class="flex items-center gap-1" x-on:click="setTheme('light')">
-                        <span x-bind:class="themeIs('light') ? 'text-violet-500 dark:text-violet-400':'text-gray-900 dark:text-white'">{{ $version->getLabel() }}</span>
+                        <span class="text-gray-800 dark:text-white">{{ $version->getLabel() }}</span>
                     </x-converge::dropdown.item>
                 @endforeach
             </x-slot:items>
