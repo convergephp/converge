7d218a8a6bbbca52255b4fd47b3e7bb0e6e3afc9
CharrafiMed <mhch5003@gmail.com>
2025-01-17 00:48:50 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ foreach (Converge::getModules() as $module) {
     $moduleId = $module->getId();
     $moduleRoutePath = $module->getRoutePath();
 
+    // when the version is present 
+    // the route name must append it a version route
+    // the route path must be changes 
+
     if ($module->hasVersions()) {
         foreach ($module->getVersions() as $version) {
             // we don't need version links
@@ -21,26 +25,24 @@ foreach (Converge::getModules() as $module) {
             }
 
             if ($version->isDefault() && $version->isQuiet()) {
-                Route::middleware(ActivateModule::class . ':' . $moduleId)->group(function () use ($moduleId, $moduleRoutePath) {
-                    Route::name($moduleId)
-                        ->get($moduleRoutePath, ModuleController::class);
+            }
 
-                    Route::name("{$moduleId}.show")
-                        ->get("{$moduleRoutePath}/{url}", FileController::class)
-                        ->where('url', '.*');
-                });
+            if ($version->isDefault() && !$version->isQuiet()) {
+            }
+
+            if (!$version->isDefault()) {
             }
 
             dump($version);
         }
     }
 
-    // Route::middleware(ActivateModule::class . ':' . $moduleId)->group(function () use ($moduleId, $moduleRoutePath) {
-    //     Route::name($moduleId)
-    //         ->get($moduleRoutePath, ModuleController::class);
+    Route::middleware(ActivateModule::class . ':' . $moduleId)->group(function () use ($moduleId, $moduleRoutePath) {
+        Route::name($moduleId)
+            ->get($moduleRoutePath, ModuleController::class);
 
-    //     Route::name("{$moduleId}.show")
-    //         ->get("{$moduleRoutePath}/{url}", FileController::class)
-    //         ->where('url', '.*');
-    // });
+        Route::name("{$moduleId}.show")
+            ->get("{$moduleRoutePath}/{url}", FileController::class)
+            ->where('url', '.*');
+    });
 }
