86e3620e46337883aba4d4175407a60d56a979c2
CharrafiMed <mhch5003@gmail.com>
2025-01-16 16:09:25 +0100

    add dropdown from fluxtor😎

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import Alpine from 'alpinejs'
+import themeSwitcher from './components/themeSwitcher';
  
 window.Alpine = Alpine
- 
+
+// control themming
+document.addEventListener("alpine:init", themeSwitcher);
+
 Alpine.start()
