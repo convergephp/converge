2c04f8cc3b879812b32e6400790545f45c39a996
CharrafiMed <mhch5003@gmail.com>
2025-01-17 22:48:32 +0100

    wip

--- resources/views/components/page/index.blade.php
+++ resources/views/components/page/index.blade.php
@@ -1,7 +1,11 @@
 <x-converge::layout>
     <div class="max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
         <x-converge::sidebar/>
-        <div class="lg:pl-[17.125rem]" x-data="tableOfContents">
+        <div 
+            class="lg:pl-[17.125rem]"
+            x-data="tableOfContents" 
+            x-on:scroll.window="handlePermalinks"
+        >
             <div data-doc class="mx-auto max-w-3xl pt-10 xl:ml-0 xl:mr-[10.5rem] xl:max-w-none xl:pr-14">
                 {{ $slot }}
             </div>
