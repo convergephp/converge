962bc0f8da2a2b74de7e01b07f998eddf1cb4cb0
CharrafiMed <mhch5003@gmail.com>
2025-01-18 14:56:02 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -10,35 +10,41 @@ use Fluxtor\Converge\Http\Controllers\FileController;
 use Fluxtor\Converge\Http\Controllers\ModuleController;
 
 foreach (Converge::getModules() as $module) {
-
-    $moduleId = $module->getId();
+    // dump($module);
     $uri = $module->getRoutePath();
+    $moduleId = $module->getId();
     // Check if the module has versions
     if ($module->hasVersions()) {
         foreach ($module->getVersions() as $version) {
+            $uri = $module->getRoutePath();
+
             if (!$version instanceof Version) {
                 continue;  // Skip non-version links
             }
-            
-            // If the version is default and quiet, we skip it (it behaves like no version)
             if ($version->isDefault() && $version->isQuiet()) {
-                continue;
+                // do nothing
             }
-            
+
             // Otherwise, we modify the URI with the version route
             if (($version->isDefault() && !$version->isQuiet()) || !$version->isDefault()) {
                 $uri .= '/' . $version->getRoute();  // Concatenate version to URI
             }
 
-            // Debugging dump for versions
+            generateRoutes($uri, $moduleId);
         }
+        continue; // explicitly go treat other module
     }
+    generateRoutes($uri, $moduleId);
     // Register the routes for the module
-    Route::middleware(ActivateModule::class . ':' . $moduleId)->group(function () use ($moduleId, $uri) {
-        Route::name($moduleId)
+}
+
+function generateRoutes(string $uri, string $id, ?string $ExcludedPatten = null)
+{
+    Route::middleware(ActivateModule::class . ':' . $id)->group(function () use ($id, $uri) {
+        Route::name($id)
             ->get($uri, ModuleController::class);
 
-        Route::name("{$moduleId}.show")
+        Route::name("{$id}.show")
             ->get("{$uri}/{url}", FileController::class)
             ->where('url', '.*');
     });
