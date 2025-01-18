b762fcedb40c6a56ede09b67678976adae0aa7d2
CharrafiMed <mhch5003@gmail.com>
2025-01-18 13:25:07 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -13,29 +13,26 @@ foreach (Converge::getModules() as $module) {
 
     $moduleId = $module->getId();
     $uri = $module->getRoutePath();
-
     // Check if the module has versions
     if ($module->hasVersions()) {
         foreach ($module->getVersions() as $version) {
             if (!$version instanceof Version) {
                 continue;  // Skip non-version links
             }
-
+            
             // If the version is default and quiet, we skip it (it behaves like no version)
             if ($version->isDefault() && $version->isQuiet()) {
                 continue;
             }
-
+            
             // Otherwise, we modify the URI with the version route
             if (($version->isDefault() && !$version->isQuiet()) || !$version->isDefault()) {
                 $uri .= '/' . $version->getRoute();  // Concatenate version to URI
             }
 
             // Debugging dump for versions
-            // dump($version);
         }
     }
-
     // Register the routes for the module
     Route::middleware(ActivateModule::class . ':' . $moduleId)->group(function () use ($moduleId, $uri) {
         Route::name($moduleId)
