538893d6b181d18daad3a38a3754bdf5d4735722
CharrafiMed <mhch5003@gmail.com>
2025-01-19 13:16:23 +0100

    fix versions trigger color

--- resources/views/components/versions/index.blade.php
+++ resources/views/components/versions/index.blade.php
@@ -12,7 +12,7 @@
 
     <div>
         <x-converge::dropdown> 
-            <x-slot:button class="text-xs leading-5 font-semibold bg-gray-200 hover:opacity-80 transition-all duration-300 dark:bg-white/[0.03] rounded-full py-1 px-3 flex items-center space-x-2 border dark:border-white/15 border-gray-900/15  dark:hover:bg-white/[0.08] text-gray-800 dark:text-gray-200 ">
+            <x-slot:button class="text-xs leading-5 font-semibold bg-white hover:opacity-80 transition-all duration-300 dark:bg-white/[0.03] rounded-full py-1 px-3 flex items-center space-x-2 border dark:border-white/15 border-gray-900/15  dark:hover:bg-white/[0.08] text-gray-800 dark:text-gray-200 ">
                   <span>{{ $usedVersionLabel }}</span>
                   <svg width="6" height="3" class="ml-2 overflow-visible" aria-hidden="true"><path d="M0 0L3 3L6 0" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path></svg>
             </x-slot:button>
