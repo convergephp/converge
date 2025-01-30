9249f36165c71dc12d5071504c4c1409169279ac
MOHAMED CHARRAFI <130717329+CharrafiMed@users.noreply.github.com>
2025-01-30 23:02:59 +0000

    chore: fix code style

--- src/Markdown/Parsers/SelfClosingBladeComponentBlockParser.php
+++ src/Markdown/Parsers/SelfClosingBladeComponentBlockParser.php
@@ -1,75 +1,31 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Fluxtor\Converge\Markdown\Parsers;
 
-use League\CommonMark\Parser\Cursor;
-use League\CommonMark\Parser\Block\BlockStart;
-use League\CommonMark\Node\Block\AbstractBlock;
-use League\CommonMark\Parser\Block\BlockContinue;
+use Exception;
 use Fluxtor\Converge\Markdown\Blocks\BladeComponentBlock;
-use League\CommonMark\Parser\MarkdownParserStateInterface;
-use League\CommonMark\Parser\Block\BlockStartParserInterface;
+use League\CommonMark\Node\Block\AbstractBlock;
 use League\CommonMark\Parser\Block\AbstractBlockContinueParser;
+use League\CommonMark\Parser\Block\BlockContinue;
 use League\CommonMark\Parser\Block\BlockContinueParserInterface;
+use League\CommonMark\Parser\Block\BlockStart;
+use League\CommonMark\Parser\Block\BlockStartParserInterface;
+use League\CommonMark\Parser\Cursor;
+use League\CommonMark\Parser\MarkdownParserStateInterface;
 
 class SelfClosingBladeComponentBlockParser extends AbstractBlockContinueParser
 {
     private BladeComponentBlock $block;
+
     private bool $closed = false;
+
     private bool $isInlineClosed = false;
 
     public function __construct()
     {
         $this->block = new BladeComponentBlock();
-    }
-
-    public function closedImmediately()
-    {
-        $this->isInlineClosed = true;
-        return $this;
-    }
-
-    public function getBlock(): AbstractBlock
-    {
-        return $this->block;
-    }
-
-    public function canHaveLazyContinuationLines(): bool
-    {
-        return true;
-    }
-
-    public function addLine(string $line): void
-    {
-        $this->block->addLine($line);
-    }
-
-    public function tryContinue(Cursor $cursor, BlockContinueParserInterface $activeBlockParser): ?BlockContinue
-    {
-        // If the block was closed on the same line, finish the parsing.
-        if ($this->isInlineClosed) {
-            $this->closed = true;
-            return BlockContinue::finished();
-        }
-
-        // If it's not self-closing and needs to be continued, check if the closing tag appears on the next line.
-        $line = $cursor->getLine();
-        $pattern = "/\/\s*>$/";
-
-        if (preg_match($pattern, $line)) {
-            $this->block->addLine($line);
-            $this->closed = true;
-            return BlockContinue::finished();
-        }
-
-        return BlockContinue::at($cursor); // Continue if the block is still open.
-    }
-
-    public function closeBlock(): void
-    {
-        if (!$this->closed) {
-            throw new \Exception("Blade component was not closed properly.");
-        }
-        $this->block->finalize();
     }
 
     public static function createBlockStartParser(): BlockStartParserInterface
@@ -94,5 +50,58 @@ class SelfClosingBladeComponentBlockParser extends AbstractBlockContinueParser
                 return BlockStart::none();
             }
         };
+    }
+
+    public function closedImmediately()
+    {
+        $this->isInlineClosed = true;
+
+        return $this;
+    }
+
+    public function getBlock(): AbstractBlock
+    {
+        return $this->block;
+    }
+
+    public function canHaveLazyContinuationLines(): bool
+    {
+        return true;
+    }
+
+    public function addLine(string $line): void
+    {
+        $this->block->addLine($line);
+    }
+
+    public function tryContinue(Cursor $cursor, BlockContinueParserInterface $activeBlockParser): ?BlockContinue
+    {
+        // If the block was closed on the same line, finish the parsing.
+        if ($this->isInlineClosed) {
+            $this->closed = true;
+
+            return BlockContinue::finished();
+        }
+
+        // If it's not self-closing and needs to be continued, check if the closing tag appears on the next line.
+        $line = $cursor->getLine();
+        $pattern = "/\/\s*>$/";
+
+        if (preg_match($pattern, $line)) {
+            $this->block->addLine($line);
+            $this->closed = true;
+
+            return BlockContinue::finished();
+        }
+
+        return BlockContinue::at($cursor); // Continue if the block is still open.
+    }
+
+    public function closeBlock(): void
+    {
+        if (! $this->closed) {
+            throw new Exception('Blade component was not closed properly.');
+        }
+        $this->block->finalize();
     }
 }
