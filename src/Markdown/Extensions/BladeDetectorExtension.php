826dc16bac5a38f18cc61e9d8718763f2d3ce245
CharrafiMed <mhch5003@gmail.com>
2025-01-30 22:55:11 +0100

    wip

--- src/Markdown/Extensions/BladeDetectorExtension.php
+++ src/Markdown/Extensions/BladeDetectorExtension.php
@@ -7,13 +7,14 @@ use Fluxtor\Converge\Markdown\Blocks\BladeComponentBlock;
 use League\CommonMark\Environment\EnvironmentBuilderInterface;
 use Fluxtor\Converge\Markdown\Renderers\BladeComponentRenderer;
 use Fluxtor\Converge\Markdown\Parsers\BladeComponentBlockParser;
+use Fluxtor\Converge\Markdown\Parsers\SelfClosingBladeComponentBlockParser;
 
 class BladeDetectorExtension implements ExtensionInterface
 {
     public  function register(EnvironmentBuilderInterface $environment): void
     {
-        $environment->addBlockStartParser(BladeComponentBlockParser::createBlockStartParser(),250);
-        $environment->addRenderer(BladeComponentBlock::class,new BladeComponentRenderer());
+        $environment->addBlockStartParser(BladeComponentBlockParser::createBlockStartParser(), 250);
+        $environment->addBlockStartParser(SelfClosingBladeComponentBlockParser::createBlockStartParser(), 250);
+        $environment->addRenderer(BladeComponentBlock::class, new BladeComponentRenderer());
     }
-
 }
