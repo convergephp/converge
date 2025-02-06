a4fb7e7803cb580082fb3dd6a3d12c58b3ccad2a
CharrafiMed <mhch5003@gmail.com>
2025-02-06 23:49:16 +0100

    wip

--- resources/views/components/dropdown/index.blade.php
+++ resources/views/components/dropdown/index.blade.php
@@ -2,43 +2,7 @@
 
 <div {{ $attributes->merge(['class'=>'flex justify-center']) }}>
     <div
-        x-data="{
-            open: false,
-            isOpen(){
-                return this.open;
-            },
-            toggle() {
-                if (this.open) {
-                    return this.close()
-                }
-
-                this.$refs.button.focus()
-
-                this.open = true
-            },
-            close(focusAfter) {
-                if (! this.open) return
-
-                this.open = false
-
-                focusAfter && focusAfter.focus()
-            },
-            handleFocusInOut(event) {
-                const panel = this.$refs.panel
-                const button = this.$refs.button
-                const target = event.target
-
-                if (panel.contains(target)||button.contains(target)) return;
-
-                const lastFocusedElement = document.activeElement
-
-                if (this.shouldCloseDropdown(button,panel,lastFocusedElement)) this.close(button);
-            },
-            shouldCloseDropdown(button, panel, lastFocusedElement) {
-                return (!button.contains(lastFocusedElement) && !panel.contains(lastFocusedElement)) &&
-                    (lastFocusedElement && (button.compareDocumentPosition(lastFocusedElement) & Node.DOCUMENT_POSITION_FOLLOWING));
-            }
-        }"
+        x-data="dropdown"
         x-on:keydown.escape.prevent.stop="close($refs.button)"
         x-on:focusin.window="handleFocusInOut($event)"
         x-id="['dropdown-button']"
