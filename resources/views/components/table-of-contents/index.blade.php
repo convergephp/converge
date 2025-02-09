35c247ecb4427170bcf3508862607afa60fbea77
MOHAMED CHARRAFI <130717329+CharrafiMed@users.noreply.github.com>
2025-02-09 12:51:41 +0100

    Revert "Fix icons issue"

--- resources/views/components/table-of-contents/index.blade.php
+++ resources/views/components/table-of-contents/index.blade.php
@@ -1,11 +1,21 @@
-<div 
-    class="right-side   fixed bottom-0 right-[max(0px,calc(50%-45rem))] top-[3.8125rem]  mt-8 hidden w-[17.5rem] overflow-y-auto pr-4 py-10 text-gray-400 xl:block" 
->
+<div class="px-3 border-dashed bg-transparent border-l-5 border-l-gray-600/10 overflow-scroll max-h-[calc(100vh-40px)]">
+    {{-- HEADER SECTION --}}
+    <div
+        class="sticky top-0 flex items-center w-full h-10 px-2 transition-colors duration-500 bg-transparent border-b rounded-lg border-b-slate-500/10 supports-backdrop-blur:bg-background-light/60 dark:bg-transparent backdrop-blur">
+        <span class="flex items-center text-lg font-semibold text-gray-800 dark:text-white gap-x-2"> <svg width="16"
+                height="16" viewBox="0 0 16 16" fill="none" stroke="currentColor" stroke-width="2"
+                xmlns="http://www.w3.org/2000/svg" class="w-3 h-3">
+                <path d="M2.44434 12.6665H13.5554" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M2.44434 3.3335H13.5554" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M2.44434 8H7.33323" stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg> On this page</span>
+    </div>
+
+    {{-- DYNAMIQUE CONTENT SECTION --}}
+    <div class="mt-4 ml-2 text-sm" x-html="$data.tableOfContent"></div>
+
+    {{-- PUBS SECTION --}}
     <div>
-        <span class="dark:text-white text-gray-800 text-lg font-semibold flex items-center gap-x-2"> <svg width="16" height="16" viewBox="0 0 16 16" fill="none" stroke="currentColor" stroke-width="2" xmlns="http://www.w3.org/2000/svg" class="h-3 w-3"><path d="M2.44434 12.6665H13.5554" stroke-linecap="round" stroke-linejoin="round"></path><path d="M2.44434 3.3335H13.5554" stroke-linecap="round" stroke-linejoin="round"></path><path d="M2.44434 8H7.33323" stroke-linecap="round" stroke-linejoin="round"></path></svg> On this page</span>
-        <div 
-            class="mt-4 ml-2 text-sm" 
-            x-html="$data.tableOfContent"
-        ></div>
+        {{-- Pubs here --}}
     </div>
 </div>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,15 +1,5 @@
 <x-converge::page>
-    <div class="relative flex items-center">
-        {{-- TOP-RIGHT  --}}
-        <div class="pointer-events-none fixed -top-48 right-[10%] h-[400px] w-[90%] rotate-[20deg] rounded-full bg-purple-500/[0.08] shadow-[50px_50px_40px_3px] shadow-violet-500/5 blur-2xl"
-            style="border-radius:58% 42% 63% 37% / 35% 31% 69% 65%;">
-        </div>
-        {{-- BOTTOM-LEFT --}}
-        <div
-            class="pointer-events-none fixed bottom-0 right-[90%] h-[100px] w-[200px] rotate-45 rounded-full bg-indigo-500/[0.12] shadow-[50px_50px_40px_3px] shadow-cyan-500/10 blur-2xl">
-        </div>
-    </div>
-    <div class=" max-w-none prose dark:prose-invert">
-        {!! $contents!!}
+    <div class="prose max-w-none dark:prose-invert">
+        {!! $contents !!}
     </div>
 </x-converge::page>
