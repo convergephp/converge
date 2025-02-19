d6a6daba5a3b95cfe38a0c64f2dc08902f3b3ef0
CharrafiMed <mhch5003@gmail.com>
2025-02-19 19:12:49 +0100

    wip on refactoring toc

--- src/TableOfContent/TocItem.php
+++ src/TableOfContent/TocItem.php
@@ -2,6 +2,27 @@
 
 namespace Fluxtor\Converge\TableOfContent;
 
-class TocItem {
-    
+use Closure;
+
+class TocItem
+{
+    protected string|Closure $label;
+
+    protected int $depth;
+
+
+    public static function make()
+    {
+        $static = resolve(static::class);
+        return $static;
+    }
+    public function getDepth(): int
+    {
+        return $this->depth;
+    }
+
+    public function getLabel(): mixed
+    {
+        return $this->label;
+    }
 }
