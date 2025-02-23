15916004729dc8eb62cad1db8cb1e0520cf950d3
CharrafiMed <mhch5003@gmail.com>
2025-02-23 23:51:44 +0100

    add fallback icon to menu items groups

---
+++ resources/views/components/icons/openable.blade.php
@@ -0,0 +1,12 @@
+<svg
+     x-data="{
+        state: false
+    }"
+    x-modelable="state"
+    {{ $attributes }}
+    class="stroke-base-content size-4 fill-none font-semibold transition-transform duration-200 ease-in-out"
+    viewBox="0 0 24 24" stroke="currentColor">
+    <path x-show="!state" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+    <path x-show="state" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+</svg>
