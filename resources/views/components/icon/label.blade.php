fae6df425f9c90d80ad5e79871eeb18d7c8233e6
CharrafiMed <mhch5003@gmail.com>
2025-02-23 22:38:32 +0100

    wip

--- resources/views/components/icon/label.blade.php
+++ resources/views/components/icon/label.blade.php
@@ -13,21 +13,12 @@
 
 <div class="mx-1 flex w-full items-center">
     @if ($isIconBefore)
-        <span
-            {{ $icon instanceof \Illuminate\View\ComponentSlot
-                ? (new \Illuminate\View\ComponentAttributeBag($icon->attributes->getAttributes()))
-                    ->merge(['class' => 'px-0.5'])
-                : new \Illuminate\View\ComponentAttributeBag(['class' => 'px-0.5']) }}
-            >
-            {!! $icon !!}
-        </span>
+        <x-converge::icon.evaluator :$icon />
     @endif
 
     <span>{{ $label }}</span>
 
     @if ($isIconAfter)
-        <span class="px-0.5" @if ($icon instanceof \Illuminate\View\ComponentSlot) {{ $icon->attributes }} @endif>
-            {!! $icon !!}
-        </span>
+        <x-converge::icon.evaluator :$icon />
     @endif
 </div>
