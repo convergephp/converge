57568b88ed9f0fdd62ccc3130f3facb96e53e129
CharrafiMed <mhch5003@gmail.com>
2025-02-23 22:57:53 +0100

    wip

--- resources/views/components/menu-items/group/index.blade.php
+++ resources/views/components/menu-items/group/index.blade.php
@@ -3,40 +3,45 @@
     <x-converge::dropdown>
         <x-slot:button>
             <x-converge::icon.label 
-                :label="$groupItem->getLabel()"
+                :label="$groupItem->getLabel()" 
                 :iconPosition="$groupItem->getIconPosition()->value"
             >
                 <x-slot:icon class="mx-0.5">
                     @if (filled($groupItem->getOpenIcon()))
-                        <span x-show="$data.isOpen()" class="px-0.5 transition-all">
+                        <span class="px-0.5 transition-all" x-show="$data.isOpen()">
                             {!! $groupItem->getOpenIcon() !!}
                         </span>
                     @endif
-        
-                    @if(filled($groupItem->getCloseIcon()))
-                        <span x-show="!$data.isOpen()" class="px-0.5 transition-all">
+
+                    @if (filled($groupItem->getCloseIcon()))
+                        <span class="px-0.5 transition-all" x-show="!$data.isOpen()">
                             {!! $groupItem->getCloseIcon() !!}
                         </span>
                     @endif
                 </x-slot:icon>
-            </x-converge::icon.label>                  
-            {{-- <svg class="ml-2 overflow-visible" aria-hidden="true" width="6" height="3">
-                <path d="M0 0L3 3L6 0" fill="none" stroke="currentColor" stroke-width="1.5"
-                    stroke-linecap="round"></path>
-            </svg> --}}
+            </x-converge::icon.label>
+            @if (blank($groupItem->getOpenIcon()) && blank($groupItem->getCloseIcon()))
+                <svg x-bind:class="{
+                    'ml-2 overflow-visible',
+                    'rotate-1' : $data.isOpen()
+                }"  aria-hidden="true" width="6" height="3">
+                    <path d="M0 0L3 3L6 0" fill="none" stroke="currentColor" stroke-width="1.5"
+                        stroke-linecap="round"></path>
+                </svg>
+            @endif
         </x-slot:button>
         <x-slot:items class="w-36">
-            @foreach($groupItem->getItems() as $item)
+            @foreach ($groupItem->getItems() as $item)
                 <x-converge::dropdown.item 
                     class="flex items-center gap-1" 
                     :href="$item->getUrl()"
                 >
-                    <x-converge::icon.label 
+                    <x-converge::icon.label
                         :label="$item->getLabel()"
                         :icon="$item->getIcon()"
-                        :iconPosition="$item->getIconPosition()->value"
+                        :iconPosition="$item->getIconPosition()->value" 
                     />
-                </x-converge::dropdown.item>            
+                </x-converge::dropdown.item>
             @endforeach
         </x-slot:items>
     </x-converge::dropdown>
