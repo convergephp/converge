d053f371f6abd906a1a00af87cfb2b3b97c7747d
CharrafiMed <mhch5003@gmail.com>
2025-02-25 19:40:49 +0100

    wip: cleaning up the markdown files

---
+++ src/SearchEngine/MarkdownParser.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Fluxtor\Converge\SearchEngine;
+
+class MarkdownParser
+{
+    protected string $contents;
+
+    public function __construct($contents)
+    {
+        $this->contents = $contents;
+    }
+
+    public static function make($contents)
+    {
+        return new static($contents);
+    }
+
+
+    public function removeCodeBlocks()
+    {
+        // get rid of fenced code blocks
+        $contents = preg_replace('/^(```|~~~)[\s\S]+?\1/m', '', $this->contents);
+
+        $contents = trim($contents);
+        // $contents = preg_replace('/[\r\n]+/', "\n", $contents);
+        
+        // dd($contents);
+        // get rid of intended code that has a tab or more than 4 spaces
+        $lines = explode("\n", $contents);
+        
+        $lines = array_filter($lines, function($line) {
+            return !empty(trim($line)); // Remove empty or just-whitespace lines
+        });
+        
+        $filteredLines = [];
+        
+        
+        foreach ($lines as $line) {
+            // Check if the line is an indented code block
+            if (preg_match('/^( {4,}|\t)/', $line)) {
+                continue; // Skip this line
+            }
+            
+            // Add non-code lines to filtered output
+            $filteredLines[] = $line;
+        }
+
+        return implode("\n", $filteredLines);
+    }
+    
+}
---
+++ src/SearchEngine/roadmap.md
@@ -0,0 +1,7 @@
+# Indexing files before search 
+
+## make the markdown has sence  
+
+### cleaning the markdown files:
+
+- remove intended && fenced code blocks
