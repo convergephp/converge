bf943b3aeb29a3329dd8cb1b7fd1987123f17559
CharrafiMed <mhch5003@gmail.com>
2025-03-01 17:56:00 +0100

    wip: search engine

--- src/SearchEngine/InvertedIndexer.php
+++ src/SearchEngine/InvertedIndexer.php
@@ -42,80 +42,34 @@ class InvertedIndexer
     {
         $token = preg_replace('/[^a-z0-9]/', '', $token);
 
-        if (empty($token) || $this->inStopWords($token)) {
+        if (empty($token) || is_numeric($token)||$this->inStopWords($token)) {
             return null;
         }
+
 
         return $token;
     }
     public function inStopWords(string $token)
     {
         $stopWords = require __DIR__ . '/stop_words.php';
-
+        
         return isset($stopWords[$token]);
-    }
-
-    public function getStopWords(): array
-    {
-        return [
-            'the',
-            'is',
-            'in',
-            'and',
-            'of',
-            'a',
-            'to',
-            'for',
-            'on',
-            'at',
-            'by',
-            'with',
-            'as',
-            'an',
-            'be',
-            'this',
-            'that',
-            'it',
-            'or',
-            'from',
-            'was',
-            'were',
-            'are',
-            'will',
-            'we',
-            'you',
-            'they',
-            'i',
-            'he',
-            'she',
-            'has',
-            'had',
-            'have',
-            'having',
-            'not',
-            'be',
-            'has',
-            'how',
-            'what',
-            'why',
-            'where',
-            'when',
-            'which',
-            'who',
-            'whose',
-            'about',
-            'through',
-            'during',
-            'before',
-            'after',
-            'under',
-            'above'
-        ];
     }
 
     protected function saveIndex()
     {
-        // Save the index to a PHP file that returns the index array
-        file_put_contents(storage_path('converge/inverted_index.php'), '<?php return ' . var_export($this->indexes, true) . ';');
+        $output = '<?php return [';
+
+        foreach ($this->indexes as $token => $headings) {
+
+            $output .= "\n    '$token' => [" . implode(', ', $headings) . "],";
+        }
+
+        $output .= "\n];";
+
+        file_put_contents(
+            storage_path('converge/inverted_index.php'),
+            $output
+        );
     }
 }
