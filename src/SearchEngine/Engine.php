f4114221afced824e3a99ead6379a92dd8b88b83
CharrafiMed <mhch5003@gmail.com>
2025-03-02 23:11:26 +0100

    feat: optimize the query processorn

--- src/SearchEngine/Engine.php
+++ src/SearchEngine/Engine.php
@@ -4,36 +4,44 @@ namespace Fluxtor\Converge\SearchEngine;
 
 class Engine
 {
-    public function search($query, bool $autoComplete = false)
+    public function search($query, bool $autoComplete = false): array
     {
-
-        $processor = new QueryProcessor($query); // stemm normalize the query and so on....
-
+        $processor = new QueryProcessor($query);
         $tokens = $processor->tokenize();
 
-        $results = [];
+        if (empty($tokens)) {
+            return [];
+        }
 
         $indexes = require storage_path('converge/inverted_index.php');
-
         $headings = require storage_path('converge/headings.php');
 
-
+        $results = [];
         $headingsIds = [];
+
         foreach ($tokens as $token) {
-            foreach (array_keys($indexes) as $index) {
-                if ($index === $token) {
-                    $headingsIds[] = $indexes[$index];
+            if (isset($indexes[$token])) {
+                foreach ($indexes[$token] as $id) {
+                    if (!isset($headingsIds[$id])) {
+                        $headingsIds[$id] = 0;
+                    }
+                    $headingsIds[$id]++;
                 }
             }
         }
 
+        if (empty($headingsIds)) {
+            return [];
+        }
 
-        foreach ($headingsIds as $ids) {
-            foreach ($ids as $id) {
+        arsort($headingsIds);
+
+        foreach (array_keys($headingsIds) as $id) {
+            if (isset($headings[$id])) {
                 $results[] = $headings[$id];
             }
         }
-        
+
         return $results;
     }
 }
