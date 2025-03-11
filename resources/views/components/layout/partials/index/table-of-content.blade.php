2882b7a390291e6897be35bf844be1630491f1ad
Ayoubhj866 <ayoubelhajji866@gmail.com>
2025-03-11 13:35:55 +0000

    wip

--- resources/views/components/layout/partials/index/table-of-content.blade.php
+++ resources/views/components/layout/partials/index/table-of-content.blade.php
@@ -3,10 +3,14 @@
 @endphp
 <div class="hidden min-w-[19rem] xl:block">
     <div class="text-base-content sticky top-24 max-h-[calc(100vh-8rem)] w-[19rem] overflow-y-auto">
-        <x-converge::table-of-contents />
-    </div>
-    {{-- Carbon ADS --}}
-    <div class="rounded-box overflow-hidden bg-transparent p-4">
-        {{ intercept(\Fluxtor\Converge\Enums\Interceptor::AFTER_TOC_CARBON_ADS) }}
+        <div class="flex flex-col">
+            <x-converge::table-of-contents />
+            {{-- Carbon ADS --}}
+            @if (filled(intercept(\Fluxtor\Converge\Enums\Interceptor::AFTER_TOC_CARBON_ADS)))
+                <div class="rounded-box overflow-hidden bg-transparent p-4">
+                    {{ intercept(\Fluxtor\Converge\Enums\Interceptor::AFTER_TOC_CARBON_ADS) }}
+                </div>
+            @endif
+        </div>
     </div>
 </div>
