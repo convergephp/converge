cd6af66be95730ff506b1844c4bac57748ae07a0
Ayoubhj866 <ayoubelhajji866@gmail.com>
2025-04-03 19:43:06 +0000

    add new layout

--- resources/views/components/table-of-contents/ul.blade.php
+++ resources/views/components/table-of-contents/ul.blade.php
@@ -5,24 +5,19 @@
 
 <ul @class([
     'ml-4' => $depth > 0,
-])
-
->
+])>
     @foreach ($headings as $heading)
-        <li 
-            class="px-2 py-1 leading-5 transition-colors duration-300 hover:text-primary"
-            x-bind:class="{'text-primary/80 ' : '#'+ $data.activeHeading === '{{ $heading->getSlug() }}'}"
-        >
-            <a href="{{ $heading->getSlug() }}">
+        <li class="hover:text-primary px-2 py-1 leading-5 transition-colors duration-300"
+            x-bind:class="{ 'text-primary/80 ': '#' + $data.activeHeading === '{{ $heading->getSlug() }}' }">
+            <a href="{{ $heading->getSlug() }}"
+               class="text-base-content hover:text-primary text-sm">
                 {{ $heading->getLabel() }}
             </a>
         </li>
 
         @if ($heading->getChildrens()->isNotEmpty())
-            <x-converge::table-of-contents.ul 
-                :headings="$heading->getChildrens()" 
-                :depth="$depth + 1"
-            />
+            <x-converge::table-of-contents.ul :headings="$heading->getChildrens()"
+                                              :depth="$depth + 1" />
         @endif
     @endforeach
 </ul>
