b26bf47ad9865d7b27d82dd9d2072cc87a59243c
Ayoubhj866 <ayoubelhajji866@gmail.com>
2025-04-17 15:01:10 +0100

    wip

--- src/Support/SidebarItemsStyles.php
+++ src/Support/SidebarItemsStyles.php
@@ -9,25 +9,25 @@ class SidebarItemsStyles
     /**
      * items like buttons
      */
-    public const STYLE1 = [
+    public const GHOST = [
         'baseClasses' => 'group/mini-link  group  w-full flex border-transparent  cursor-pointer items-center space-x-3 px-2 py-1  text-base-content  hover:text-primary duration-200',
         'activeClasses' => '!border-l text-primary',
         'nestedClasses' => '!border-primary hover:border-l',
     ];
 
-    public const STYLE2 = [
+    public const WHISPER = [
         'baseClasses' => 'group/mini-link  group   w-full mt-1  flex  cursor-pointer items-center  space-x-3 py-2 px-2  text-base-content  hover:bg-primary/10 duration-200',
         'activeClasses' => 'bg-primary/10',
         'nestedClasses' => '',
     ];
 
-    public const STYLE3 = [
+    public const ECHO = [
         'baseClasses' => 'group/mini-link  group  w-full mt-1  flex  cursor-pointer items-center  space-x-3 py-2 px-2  text-base-content  hover:text-primary duration-200',
         'activeClasses' => 'text-primary pl-5 transition-all duration-500',
         'nestedClasses' => '',
     ];
 
-    public const STYLE4 = [
+    public const AURA = [
         'baseClasses' => 'group/mini-link  group  w-full mt-1 rounded-field  flex  cursor-pointer items-center  space-x-3 py-2 px-2  text-base-content  hover:bg-primary/10 duration-200',
         'activeClasses' => 'bg-primary/10 text-primary border-l border-transparent',
         'nestedClasses' => '!rounded-l-none !border-l-primary',
