e2d001b7f195d206e79af0f71427d217f07756d3
CharrafiMed <mhch5003@gmail.com>
2025-04-19 17:19:28 +0200

    making view interceptor scoped to specific module && global to all modules

--- src/Views/ViewInterceptor.php
+++ src/Views/ViewInterceptor.php
@@ -13,58 +13,47 @@ use function Fluxtor\Converge\converge;
 
 class ViewInterceptor
 {
-    // act as global hooks
+    // global hooks
     protected array $viewPoints = [];
+    
+    // scoped to one module hooks 
+    protected array $pointsLookup = [];
 
-    // module scoped hooks
+    protected array $reflectionCache = [];
 
-    protected array $contextualPoints = [];
-
-    protected array $withinModules = [];
-
-    public function registerViewInterceptor(Interceptor $name, Closure $interceptor, ?string $addModule = null): void
+    public function registerViewInterceptor(Interceptor $name, Closure $interceptor, ?string $addModule = null)
     {
 
-        // is likely getting registred from the sevice provider and we need to make globally
         if (is_null($addModule)) {
             $this->viewPoints[$name->value] = $interceptor;
         } else {
-            $this->contextualPoints[] = [
-                'name' => $name->value,
-                'id' => $addModule
-            ];
+            $this->pointsLookup[$addModule][$name->value] = $interceptor;
         }
-
-
-        // if the add module is set so we need a way to mark that is belong to this module
-        // grab the module 
-
-        // $this->withinModules = array_unique([...$this->withinModules, $addModule]);
+        return $this;
     }
 
     public function render($point, ?callable $context = null)
     {
 
+        $starts = hrtime(true);
         $activeModuleId = converge()->getId();
-        $shouldConsumed = [];
-        foreach ($this->contextualPoints as $contextualPoint) {
-            if ($contextualPoint['id'] === $activeModuleId) {
-                $shouldConsumed[] = $contextualPoint;
-            }
+        $view = $this->pointsLookup[$activeModuleId][$point->value] ?? null;
+
+        if (! $view && isset($this->viewPoints[$point->value])) {
+            $view = $this->viewPoints[$point->value];
         }
 
-        // there is no global points 
-        if (! isset($this->viewPoints[$point->value]) && !count($shouldConsumed)) {
-            return null;
+        if (is_null($view)) return null;
+
+
+        $key = spl_object_hash($view);
+
+        if (! isset($this->reflectionCache[$key])) {
+            $this->reflectionCache[$key] = new ReflectionFunction($view);
         }
 
+        $reflector = $this->reflectionCache[$key];
 
-        // THERE is no contextual points
-
-
-        $view = $this->viewPoints[$point->value];
-
-        $reflector = new ReflectionFunction($view);
 
         if (is_null($context) || $reflector->getNumberOfParameters() === 0) {
             return value($view);
