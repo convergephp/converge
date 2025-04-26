f4fb089df5c543e95d997891e8265a145b5778e2
CharrafiMed <mhch5003@gmail.com>
2025-04-26 23:15:52 +0200

    wip

--- resources/views/components/search/result-item.blade.php
+++ resources/views/components/search/result-item.blade.php
@@ -3,7 +3,7 @@
     <a x-bind:href="result.url"
        @class([
            'justify-start scroll-mt-9 mt-1  group bg-base-100 py-2 px-3 duration-300 border border-transparent w-full transition-colors rounded-field focus:bg-base-100/60 focus:border focus-visible:outline-none focus:border-primary/30 hover:bg-base-300/60 hover:border-primary/20 hover:border flex justify-between items-center',
-           'p-3',
+           'p-3'
        ])>
         <div class="flex flex-col items-start justify-start text-start">
             <h4 x-html="result.title"
